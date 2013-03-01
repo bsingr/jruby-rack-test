ceae70c50eb0b01f969cb22bc0c3e9e7ec9866f3
Jens Bissinger <mail@jens-bissinger.de>
2013-03-01 20:31:00 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrubyRackTest::Application.config.secret_token = '560a90fd9a316f1a1421042d44e40904e10d74739451593641d39546b7ca47ad731dfa76c711ce0e92ced6f64352d3d47d753290c4018254a58a6d750bcbae0a'
